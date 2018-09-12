08763e951cff6fc92e8be3eec591547ca1bae9ba
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2018-09-12 17:53:31 -0300

    A

--- 2018_2/Trabalho2/SumilBin.cpp
+++ 2018_2/Trabalho2/SumilBin.cpp
@@ -9,14 +9,19 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string>
 #include <iostream>        
 #include <bitset>
+#include <fstream>
+#include <sstream>
+#include <cstring>
 
-#define MEM_SIZE 4096 
-int32_t mem[MEM_SIZE];
+#define MEM_SIZE 16384 // 4096 não é suficiente para armazenar texto e dado na memória
+int32_t mem[MEM_SIZE];	
+int32_t regs[32];
 int16_t k16;
-uint26_t k26;
+uint32_t k26;
 
 int pos;
 std::bitset<6> opcode;
@@ -27,6 +32,8 @@ std::bitset<5> shamt;
 std::bitset<6> funct;
 uint32_t pc;
 uint32_t ri;
+int32_t hi;
+int32_t lo;
 
 using namespace std;
 
@@ -46,14 +53,8 @@ enum FUNCT{
 	MFLO=0x12, ADDU=0x21, SLTU=0x2b
 };
 
-void dump_mem(uint32_t 	, uint32_t size){			// Função que imprime o conteúdo da memória no formato hexa, palavra por palavra
-	size /= 4;
-	size += 	;
-
-	for(int i = add; i < size; ++i)
-	{
-		cout << "mem[" << i << "] = " << hex << mem[i] << endl;
-	}
+void dump_mem(int start, int end, char format){			// Função que imprime o conteúdo da memória no formato hexa, palavra por palavra
+	
 }
 
 void dump_reg(char format){						// Função que imprime conteudo do registrador
@@ -76,8 +77,8 @@ int32_t lh(uint32_t address, int16_t kte){
 	int16_t dado = mem[address];
 	int32_t retorno;
 
-	if(kte == 0) dado = mem[address]&0x0000FFFF;			// Mascara aplicada para conseguir os 2 bytes menos significativos
-	if(kte == 2) dado = (mem[address] >> 16)&0x0000FFFF;	// Mascara aplicada para conseguir os 2 bytes mais significativos
+	if(kte%4 == 0) dado = mem[address]&0x0000FFFF;			// Mascara aplicada para conseguir os 2 bytes menos significativos
+	if(kte%4 == 2) dado = (mem[address] >> 16)&0x0000FFFF;	// Mascara aplicada para conseguir os 2 bytes mais significativos
 															// após serem deslocados para a direita para se tornarem os menos significativos
 
 	cout << "Decimal: " << dec << dado << endl << "Hexadecimal " << hex << dado << endl;
@@ -90,8 +91,8 @@ uint32_t lhu(uint32_t address, int16_t kte){
 	uint16_t dado = mem[address];
 	uint32_t retorno;
 
-	if(kte == 0) dado = mem[address]&0x0000FFFF;			// Mascara aplicada para conseguir os 2 bytes menos significativos
-	if(kte == 2) dado = (mem[address] >> 16)&0x0000FFFF;	// Mascara aplicada para conseguir os 2 bytes mais significativos
+	if(kte%4 == 0) dado = mem[address]&0x0000FFFF;			// Mascara aplicada para conseguir os 2 bytes menos significativos
+	if(kte%4 == 2) dado = (mem[address] >> 16)&0x0000FFFF;	// Mascara aplicada para conseguir os 2 bytes mais significativos
 															// após serem deslocados 16 bites para a direita para se tornarem os menos significativos
 
 	cout << "Decimal: " << dec << dado << endl;
@@ -104,28 +105,28 @@ int32_t lb(uint32_t address, int16_t kte){
 	int32_t dado, retorno;
 	uint32_t temp = mem[address];
 
-	if(kte == 0){
+	if(kte%4 == 0){
 		temp = ((temp << 16) << 8);		// Deslocamento de todos os bitts 32 bits para a esquerda, para apagar todo o conteudo
 										// da memória exceto o byte menos significativo.
 		temp = ((temp >> 16) >> 8);		// Deslocamento de todos os bitts 32 bits para a direita, para retornar o byte menos
 										// significativo para sua posição inicial.
 		dado = temp;
 	}
-	if(kte == 1){
+	if(kte%4 == 1){
 		temp = ((temp << 16));			// Deslocamento de todos os bitts 16 bits para a esquerda, para apagar todo o conteudo
 										// da memória exceto os 2 bytes menos significativos.
 		temp = ((temp >> 16) >> 8);;	// Deslocamento de todos os bitts 32 bits para a direita, para apagar o byte menos significativo
 										// da memória inicial, deixando o 2o byte menos significativo na posição 0 da memória.
 		dado = temp;
 	}
-	if(kte == 2){
+	if(kte%4 == 2){
 		temp = ((temp << 8));			// Deslocamento de todos os bitts 8 bits para a esquerda, para apagar todo o conteudo
 										// da memória exceto o byte mais significativo.
 		temp = ((temp >> 16) >> 8);		// Deslocamento de todos os bitts 32 bits para a direita, para apagar os 2 byte menos significativos
 										// da memória inicial, deixando o 2o byte mais significativo na posição 0 da memória.
 		dado = temp;
 	}
-	if(kte == 3){
+	if(kte%4 == 3){
 		temp = ((temp >> 16) >> 8);		// Deslocamento de todos os bitts 32 bits para a direita, para apagar todo o conteudo
 										// da memória exceto o byte mais significativo, deixando-o na posição 0 da memória.
 		dado = temp;
@@ -140,19 +141,19 @@ int32_t lbu(uint32_t address, int16_t kte){
 	address = address/4;
 	uint32_t dado = mem[address], retorno;
 
-	if(kte == 0){
+	if(kte%4 == 0){
 		dado = ((dado << 16) << 8);
 		dado = ((dado >> 16) >> 8);
 	}
-	if(kte == 1){
+	if(kte%4 == 1){
 		dado = ((dado << 16));
 		dado = ((dado >> 16) >> 8);
 	}
-	if(kte == 2){
+	if(kte%4 == 2){
 		dado = ((dado << 8));
 		dado = ((dado >> 16) >> 8);
 	}
-	if(kte == 3){
+	if(kte%4 == 3){
 		dado = ((dado >> 16) >> 8);
 	}
 
@@ -165,9 +166,9 @@ void sw(uint32_t address, int16_t kte, int32_t dado){
 	address = address/4;
 
 	
-	if(kte == 1) dado = (dado << 8);			// Caso kte seja 1, a word será deslocada para a direita, sendo aramzenada a partir da posição 1.
-	if(kte == 2) dado = (dado << 16);			// Caso kte seja 2, a word será deslocada para a direita, sendo aramzenada a partir da posição 2.
-	if(kte == 3) dado = ((dado << 16) << 8);	// Caso kte seja 3, a word será deslocada para a direita, sendo aramzenada a partir da posição 3.
+	if(kte%4 == 1) dado = (dado << 8);			// Caso kte seja 1, a word será deslocada para a direita, sendo aramzenada a partir da posição 1.
+	if(kte%4 == 2) dado = (dado << 16);			// Caso kte seja 2, a word será deslocada para a direita, sendo aramzenada a partir da posição 2.
+	if(kte%4 == 3) dado = ((dado << 16) << 8);	// Caso kte seja 3, a word será deslocada para a direita, sendo aramzenada a partir da posição 3.
 
 
 	mem[address] = dado;
@@ -177,24 +178,24 @@ void sh(uint32_t address, int16_t kte, int16_t dado){
 	address = address/4;
 	uint32_t temp = (uint32_t)dado, backup;
 	
-	if(kte == 0){
+	if(kte%4 == 0){
 		temp = temp&0x0000FFFF;					// Máscara para corrigir bug.
 		backup = mem[address]&0xFFFFFF00;		// Backup do que já existia na memória, exceto na
 												// posição que será introduzido um novo dado.
 	}
-	if(kte == 1){
+	if(kte%4 == 1){
 		temp = (temp << 8);
 		temp = temp&0x00FFFF00;					// Máscara para corrigir bug.
 		backup = mem[address]&0xFFFF00FF;		// Backup do que já existia na memória, exceto na
 												// posição que será introduzido um novo dado.
 	}
-	if(kte == 2){
+	if(kte%4 == 2){
 		temp = (temp << 16);
 		temp = temp&0xFFFF0000;					// Máscara para corrigir bug.
 		backup = mem[address]&0xFF00FFFF;		// Backup do que já existia na memória, exceto na
 												// posição que será introduzido um novo dado.
 	}
-	if(kte == 3){
+	if(kte%4 == 3){
 		temp = ((temp << 16) << 8);
 		temp = temp&0xFF000000;					// Máscara para corrigir bug.
 		backup = mem[address]&0x00FFFFFF;		// Backup do que já existia na memória, exceto na
@@ -209,27 +210,27 @@ void sb(uint32_t address, int16_t kte, int8_t dado){
 	address = address/4;
 	uint32_t temp = (uint32_t)dado, backup;
 	
-	if(kte == 0){
+	if(kte%4 == 0){
 		backup = mem[address]&0xFFFFFF00;		// Backup do que já existia na memória, exceto na
 												// posição que será introduzido um novo dado.
 		temp = temp&0x000000FF;					// Máscara para corrigir bug.
 	}
 
-	if(kte == 1){
+	if(kte%4 == 1){
 		temp = (temp << 8);
 		temp = temp&0x0000FF00;					// Máscara para corrigir bug.
 		backup = mem[address]&0xFFFF00FF;		// Backup do que já existia na memória, exceto na
 												// posição que será introduzido um novo dado.
 	}
 
-	if(kte == 2){
+	if(kte%4 == 2){
 		temp = (temp << 16);
 		temp = temp&0x00FF0000;					// Máscara para corrigir bug.
 		backup = mem[address]&0xFF00FFFF;		// Backup do que já existia na memória, exceto na
 												// posição que será introduzido um novo dado.
 	}
 
-	if(kte == 3){
+	if(kte%4 == 3){
 		temp = ((temp << 16) << 8);
 		temp = temp&0xFF000000;					// Máscara para corrigir bug.
 		backup = mem[address]&0x00FFFFFF;		// Backup do que já existia na memória, exceto na
@@ -241,32 +242,29 @@ void sb(uint32_t address, int16_t kte, int8_t dado){
 }
 
 void execute(){			// Função que executa a instrução
-	if(opcode == )
+
 }
 
 void decode(){			// Função que decodifica a instrução
-	bitset<32> inst(ri);
-	bitset<6> aux;
-	for(int i = 0; i < 32; ++i){
-		if(i < 6)funct[i] = inst[i];
-		else if(i < 11)shamt[i - 6] = inst[i];
-		else if(i < 16)rt[i - 11] = inst[i];
-		else if(i < 21)rs[i - 16] = inst[i];
-		else if(i < 26)rd[i - 21] = inst[i];
-		else opcode[i-26] = inst[i];
-	}
-
+	funct = ri & 0x3F;
+	shamt = (ri >> 6) & 0x1F;
+	rd = (ri >> 11) & 0x1F;
+	rt = (ri >> 16) & 0x1F;
+	rd = (ri >> 11) & 0x1F;
+	opcode = (ri >> 26) & 0x3F;
+	k16 = ri & 0xFFFF;
+	k26 = ri & 0x3FFFFFF;
 }
 
 void fetch(){			// Função que busca a instrução
 	if(pc == 0){
 		ri = mem[0];
-		pc = mem[4];
-		pos = 4;
+		pc = mem[1];
+		pos = 1;
 	}
 	else{
 		ri = pc;
-		pos += 4;
+		++pos;
 		pc = mem[pos];
 	}
 
@@ -280,13 +278,32 @@ void step(){		// Função que executa uma instrução do MIPS
 
 void salvar_arquivo(){		// Função para pegar instruções do arquivo binário e armazenar no vetor mem[]
 	ifstream instrucoes;
-	instrucoes.open("bin.txt");
-	instrucoes
+	instrucoes.open("entrada.txt");
+	int pos = 0;
+	unsigned long long int valor;
+	string temp;
+	stringstream aux;
+
+	if(!instrucoes.is_open()){						//Verifica se encontrou o arquivo de entrada
+		cout << "Arquivo nao encontrado" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	while(instrucoes.good()){						// Lê o arquivo até o final
+		instrucoes >> temp;
+		valor = strtoul(temp.c_str(), NULL, 2);		// Transforma temp em um int de base 2
+		mem[pos] = (int) valor;						// Passa o conteudo da stringstream para o vetor da memória
+		++pos;
+	}
+	instrucoes.close();
 }
 
 int main(int argc, char *argv[])
 {
-	step();
+	regs[29] = 0x3ffc;
+	regs[28] = 0x1800;
+	salvar_arquivo();
+	//step();
 
     return 0;
 }
