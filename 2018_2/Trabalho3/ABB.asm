518944d7d7c16445367cb5f412e3ba064737dbdc
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2018-09-30 17:41:12 -0300

    Final

--- 2018_2/Trabalho3/ABB.asm
+++ 2018_2/Trabalho3/ABB.asm
@@ -19,7 +19,8 @@ numnos: "\nQuantidade de nós na árvore: "
 .text
 
 main:
-	li $s7, -1
+	li $t1, -1
+	sw $t1, 0($gp)
 
 menu:
 	li $v0, 4
@@ -64,13 +65,17 @@ input:
 	la $a0, nl
 	syscall
 	
-	j entrada
+	j input
 	
 sai:
 	li $v0, 10
 	syscall
 	
 insercao:
+	li $t1, -1
+	lw $t2, 0($gp)
+	move $t3, $gp
+	
 	li $v0, 4
 	la $a0, adddado
 	syscall
@@ -80,10 +85,11 @@ insercao:
 	
 	move $s0, $v0
 	
-	bne $s7, $t2, naovazia
+	bne $t1, $t2, naovazia
 	sw $s0, 0($gp)
-	sw $s7, 4($gp)		# Mostra que não há filhos à esquerda
-	sw $s7, 8($gp)		# Mostra que não há filhos à direita
+	addiu $a1, $zero, 1	# Armazena quantos nós foram adicionados à árvore
+	sw $t1, 4($gp)		# Mostra que não há filhos à esquerda
+	sw $t1, 8($gp)		# Mostra que não há filhos à direita
 	
 	j menu
 	
@@ -110,15 +116,18 @@ inseredireita:
 	sll $t5, $t5, 2
 	add $t3, $gp, $t5	# Endereço do nó que analizaremos (atual) armazenado em t3
 	
+	
 	sub $t5, $t3, $gp
 	srl $t5, $t5, 2
 	add $t5, $t5, $t5
 	addiu $t5, $t5, 2
 	sll $t5, $t5, 2
-	add $t6, $gp, $t5	# Endereço do filho a direita	
+	add $t6, $gp, $t5	# Endereço do filho a direita
 	
+	
+	sub $t5, $t3, $gp
 	srl $t5, $t5, 2
-	subi $t5, $t5, 2
+	add $t5, $t5, $t5
 	addiu $t5, $t5, 1
 	sll $t5, $t5, 2
 	add $t7, $gp, $t5	# Endereço do filho a esquerda
@@ -127,6 +136,7 @@ inseredireita:
 	bne $t1, $t2, naovazia
 	
 	sw $s0, 0($t3)
+	addiu $a1, $a1, 1	# Armazena quantos nós foram adicionados à árvore
 	sw $t1, 0($t6)		# Mostra que não há filhos à esquerda
 	sw $t1, 0($t7)		# Mostra que não há filhos à direita
 	
@@ -134,6 +144,7 @@ inseredireita:
 	
 	
 insereesquerda:
+	
 	sub $t5, $t3, $gp
 	srl $t5, $t5, 2
 	add $t5, $t5, $t5
@@ -141,15 +152,18 @@ insereesquerda:
 	sll $t5, $t5, 2
 	add $t3, $gp, $t5	# Endereço do nó que analizaremos (atual) armazenado em t3
 	
+	
 	sub $t5, $t3, $gp
 	srl $t5, $t5, 2
 	add $t5, $t5, $t5
 	addiu $t5, $t5, 2
 	sll $t5, $t5, 2
-	add $t6, $gp, $t5	# Endereço do filho a direita	
+	add $t6, $gp, $t5	# Endereço do filho a direita
 	
+	
+	sub $t5, $t3, $gp
 	srl $t5, $t5, 2
-	subi $t5, $t5, 2
+	add $t5, $t5, $t5
 	addiu $t5, $t5, 1
 	sll $t5, $t5, 2
 	add $t7, $gp, $t5	# Endereço do filho a esquerda
@@ -158,6 +172,7 @@ insereesquerda:
 	bne $t1, $t2, naovazia
 	
 	sw $s0, 0($t3)
+	addiu $a1, $a1, 1	# Armazena quantos nós foram adicionados à árvore
 	sw $t1, 0($t6)		# Mostra que não há filhos à esquerda
 	sw $t1, 0($t7)		# Mostra que não há filhos à direita
 	
@@ -180,7 +195,7 @@ busca:
 	
 procura:lw $t2, 0($t3)
 	
-	beq $t2, $t1, ehmenosum
+	beq $t2, $s7, ehmenosum
 	
 	beq $s0, $t2, encontrado
 	slt $t4, $t2, $s0 
@@ -216,7 +231,7 @@ ehmenosum:
 	add $t6, $gp, $t5	# Caso s0 seja -1, conferir se é dado -1 ou -1 que corresponde a um nó vazio
 	lw $t7, 0($t6)
 	
-	bne $t1, $t7, naoencontrado	
+	bne $s7, $t7, naoencontrado	
 
 encontrado:	
 	li $v0, 4
@@ -251,60 +266,11 @@ nos:
 	beq $t4, 1, arvorevazia
 	
 	move $a0, $gp
+	addi $v0, $zero, 1
+	move $v1, $zero
 	jal getNumNodes
-
-getNumNodes:
-    	empilha_tudo
-   	li $s0, 1	# Acumulador onde fica a quantidade de nós calcula o endereço do filho da esquerda
-   	subu $s1, $a0, $gp
-   	addiu $t
-    	multu $s1, $s1, 2
-    addiu $s1, $s1, 1
-    addu $s1, $gp, $s1
-
-    ; calcula o endereço do filho da direita
-    addi $s2, $s1, 1
-    
-    lw $t1, ($s1) ; carrega o valor do filho da esquerda
-    bne $t1, -1, testaEsquerda
-    voltaEsquerda:
-
-    lw $t2, ($s2) ; carrega o valor do filho da direita
-    bne $t2, -1, testaDireita
-    voltaDireita:
-    
-    move $v0, $s0
-    desempilha_tudo
-    jr $ra
-
-    testaEsquerda:
-    move $a0, $s1
-    jal getNumNodes
-    add $s0, $s0, $v0
-    j voltaEsquerda
-
-    testaDireita:
-    move $a0, $s1
-    jal getNumNodes
-    add $s0, $s0, $v0
-    j voltaDireita
-	entradireita:
-		sub $t5, $t3, $gp
-		srl $t5, $t5, 2
-		add $t5, $t5, $t5
-		addiu $t5, $t5, 2
-		sll $t5, $t5, 2
-		add $t3, $gp, $t5
-		move $a1, $t3
-		
 	
-	entraesquerda:
-		sub $t5, $t3, $gp
-		srl $t5, $t5, 2
-		add $t5, $t5, $t5
-		addiu $t5, $t5, 1
-		sll $t5, $t5, 2
-		add $t3, $gp, $t5
+	add $a1, $v0, $v1
 	
 	li $v0, 4
 	la $a0, numnos
@@ -315,3 +281,71 @@ getNumNodes:
 	syscall
 	
 	j menu
+
+getNumNodes:
+    	
+   	li $s0, 1		# Acumulador onde fica a quantidade de nós calcula o endereço do filho da esquerda
+   	subu $s1, $a0, $gp
+   	srl $s1, $s1, 2
+    	add $s1, $s1, $s1
+    	addiu $s1, $s1, 1
+    	addiu $s2, $s1, 1
+    	sll $s1, $s1, 2
+    	sll $s2, $s2, 2
+   	addu $s1, $gp, $s1	# Calcula o endereço do filho da direita
+   	addu $s2, $gp, $s2	# Calcula o endereço do filho da esquerda
+   	
+    	addiu $sp, $sp, -20
+    	sw $ra, 0($sp)
+    	sw $s0, 4($sp)
+    	sw $s1, 8($sp)
+    	sw $s2, 12($sp)
+    	sw $a0, 16($sp)
+   	
+    	lw $t1, ($s1)		# Carrega o valor do filho da esquerda
+    	bne $t1, -1, testaEsquerda
+    	lw $t2, ($s2)		# Carrega o valor do filho da direita
+    	bne $t2, -1, testaDireita
+    	
+    	lw $ra, 0($sp)
+    	lw $s0, 4($sp)
+    	lw $s1, 8($sp)
+    	lw $s2, 12($sp)
+    	lw $a0, 16($sp)
+    	addiu $sp, $sp, 20
+    	jr $ra
+	
+voltaEsquerda:    	
+    	lw $ra, 0($sp)
+    	lw $s0, 4($sp)
+    	lw $s1, 8($sp)
+    	lw $s2, 12($sp)
+    	lw $a0, 16($sp)
+    	
+	lw $t2, ($s2)		# Carrega o valor do filho da direita
+    	bne $t2, -1, testaDireita
+	
+voltaDireita:    	
+    	lw $ra, 0($sp)
+    	lw $s0, 4($sp)
+    	lw $s1, 8($sp)
+    	lw $s2, 12($sp)
+    	lw $a0, 16($sp)
+    	addiu $sp, $sp, 20
+    	
+    	jr $ra
+
+testaEsquerda:    
+    	
+    	move $a0, $s1
+    	beqz $t1, pula
+    	jal getNumNodes
+pula:  	addi $v1, $v1, 1
+    	j voltaEsquerda
+
+testaDireita:    
+    	
+    	move $a0, $s2
+    	jal getNumNodes
+    	addi $v0, $v0, 1
+    	j voltaDireita
