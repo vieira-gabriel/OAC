41a30e3470b4661b8d9a6d87ef895df9690d5412
Arthur Lima <arthurlima96@hotmail.com>
2018-12-10 11:41:49 -0400

    Update extsgn.vhd

--- 2018_2/TrabalhoFinal/mips_multi/extsgn.vhd
+++ 2018_2/TrabalhoFinal/mips_multi/extsgn.vhd
@@ -6,11 +6,12 @@ use work.mips_pkg.all;
 entity extsgn is
 	generic (
 		IN_SIZE : natural := 16;
-		OUT_SIZE : natural := 32	
+		OUT_SIZE : natural := 32;
+		SEL_SIZE : natural := 1
 		);
 	port (
 		input : in std_logic_vector(IN_SIZE-1 downto 0);
-		ext_type: in std_logic_vector(1 downto 0); 
+		ext_type: in std_logic_vector(SEL_SIZE downto 0); 
 		output: out std_logic_vector(OUT_SIZE-1 downto 0)
 		);
 end entity;
@@ -18,7 +19,7 @@ end entity;
 architecture wires of extsgn is
 signal tmp : std_logic_vector(OUT_SIZE-1 downto 0);
 begin
-	if ext_type = "00" then
+	if ext_type = "00" or "0" then
 		output <= tmp;
 		tmp(IN_SIZE-1 downto 0) <= input;
 		tmp(OUT_SIZE-1 downto IN_SIZE) <= (others => input(IN_SIZE-1));
